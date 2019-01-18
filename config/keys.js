988dea3228438dfd352bc45bf811fcc52752eca8
Aedirn <byroninjeeli@gmail.com>
2019-01-18 17:20:49 -0500

    commit

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    connection: {
-        host: 'ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'yd382k4x79sjfjtq',
-        password: 'xgtn8ztz9ciz0k1r',
-        database: 'j2g9js7aiwk4gkdh'
-    }
-}
