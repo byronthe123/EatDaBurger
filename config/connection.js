fcf65bf1b6a2f77f3c72070597896d5f96571bab
Aedirn <byroninjeeli@gmail.com>
2019-01-19 10:25:43 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,32 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 const keys = require('./keys');
+let connection;
 
-const connection = mysql.createConnection({
-  host: keys.connection.host,
-  port: 3306,
-  user: keys.connection.user,
-  password: keys.connection.password,
-  database: keys.connection.database
-});
+// const connection = mysql.createConnection({
+//   host: keys.connection.host,
+//   port: 3306,
+//   user: keys.connection.user,
+//   password: keys.connection.password,
+//   database: keys.connection.database
+// });
+
+if(process.env.JAWSDB_URL) {
+  connectionn = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    // host: keys.host,
+    // port: 3306,
+    // user: keys.user,
+    // password: keys.password,
+    // database: keys.database
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "byron",
+    database: "burgers_db"
+  });
+}
 
 // Make connection:
 connection.connect(function(err) {
